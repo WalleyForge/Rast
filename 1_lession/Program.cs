7a477e6acd4bc6231e04633d32c93b5dfef123e9
WalleyForge <WalleyForge@users.noreply.github.com>
2021-01-19 17:05:44 +0500

    class

--- 1_lession/Program.cs
+++ 1_lession/Program.cs
@@ -10,8 +10,26 @@ namespace _1_lession
     {
         static void Main(string[] args)
         {
+<<<<<<< Updated upstream
             Console.WriteLine("Привет, это 1 урок");
             Console.ReadLine();
         }
+=======
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.s = '*';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 5;
+            p2.y = 10;
+            p2.s = '$';
+            p2.Draw();
+
+            Console.ReadLine();
+        }
+       
+>>>>>>> Stashed changes
     }
 }
